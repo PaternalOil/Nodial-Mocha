d7debc4020ed9312c922a4fd9dbc9eb63d8ed562
Patternlol <104591560+PaternalOil@users.noreply.github.com>
2022-12-12 16:57:08 +0000

    index.html has been updated with CSS and forum elements

--- index.js
+++ index.js
@@ -1,17 +1,13 @@
 var fs = require('fs');
 var http = require('http');
 
-fs.appendFile('dat.txt', '_DATA_', function (err) {
+fs.appendFile('dat.txt', '', function (err) {
   if (err) throw err;                
 });
 
-// Creates server using req http:
 http.createServer(function (req, res) {
-  // Filesystem  function reads index.html:
   fs.readFile('index.html', function(err, data) {
-  // Prepares to write html code to server:
   res.writeHead(200, {'Content-Type': 'text/html'});
-  // Write data from index.html
   res.end(data);
   });
-}).listen(8080);
+}).listen(8000);
