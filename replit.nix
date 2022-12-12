2da4222660a5ebbbf7f60efc8ae8485222f9118f
Patternlol <104591560+PaternalOil@users.noreply.github.com>
2022-12-12 09:49:22 -0500

    Removed SSH to stop infinite buffering

--- replit.nix
+++ replit.nix
@@ -1,10 +1,8 @@
 { pkgs }: {
 	deps = [
 		pkgs.openssh_with_kerberos
-  pkgs.openssh_with_kerberos
-  pkgs.openssh_with_kerberos
-  pkgs.sudo
-  pkgs.nodejs-18_x
+        pkgs.sudo
+        pkgs.nodejs-18_x
         pkgs.nodePackages.typescript-language-server
         pkgs.nodePackages.yarn
         pkgs.replitPackages.jest
