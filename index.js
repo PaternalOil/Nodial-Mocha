e6eed40bd34933a3d29fd965354e69732777fb71
Patternlol <104591560+PaternalOil@users.noreply.github.com>
2022-12-12 14:47:18 +0000

    Init Com

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-// fs - File System
-// os - Operating System
-// events
-// https
+var fs = require('fs');
+var http = require('http');
 
-function Welcome(name){
+fs.appendFile('dat.txt', '_DATA_', function (err) {
+  if (err) throw err;                
+});
 
-  console.log("Hello, "+name+".");
-  
-}
-
-Welcome("Jon"); 
+// Creates server using req http:
+http.createServer(function (req, res) {
+  // Filesystem  function reads index.html:
+  fs.readFile('index.html', function(err, data) {
+  // Prepares to write html code to server:
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  // Write data from index.html
+  res.end(data);
+  });
+}).listen(8080);
--- replit.nix
+++ replit.nix
@@ -1,6 +1,10 @@
 { pkgs }: {
 	deps = [
-		pkgs.nodejs-18_x
+		pkgs.openssh_with_kerberos
+  pkgs.openssh_with_kerberos
+  pkgs.openssh_with_kerberos
+  pkgs.sudo
+  pkgs.nodejs-18_x
         pkgs.nodePackages.typescript-language-server
         pkgs.nodePackages.yarn
         pkgs.replitPackages.jest
