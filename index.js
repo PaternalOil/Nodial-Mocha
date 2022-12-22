7aa562041b0ed4a45972eecf207ed56a7c6d8a29
Patternlol <104591560+PaternalOil@users.noreply.github.com>
2022-12-22 01:44:28 +0000

    Starting over again

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const bycrypt = require('bycrypt');
-const passport = require('passport');
-
-
-const users = []
-
-app.set('view engine', 'ejs');
-app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.render('index.ejs', {name: 'James'});
+  app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
 });
 
-app.get( '/login', (req, res) => {
-  res.render('login.ejs');
-})
-
-app.get( '/register', (req, res) => {
-  res.render('register.ejs');
-})
-
-app.post('/register', (req, res) => {
-  try{
-    const hashedpassword = await bycrypt.hash(req.body.password, 10);
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.name,
-      password: hashedpassword,
-      email: req.body.email
-    });
-    res.redirect('/login');
-  } catch {
-    redirect('/register');
-  }
-  console.log(users)
-})
-
-app.listen(3000);
+app.listen(3000, () => { console.log('server started'); });
