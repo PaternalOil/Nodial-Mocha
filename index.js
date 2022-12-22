2e152cbdb470af20bdbdaf53aee61a067a3e59b3
Patternlol <104591560+PaternalOil@users.noreply.github.com>
2022-12-22 02:31:59 +0000

    nodial

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-  app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'));
-});
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => { res.sendFile(path.join(__dirname + '/index.html')); });
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 
-app.listen(3000, () => { console.log('server started'); });
+app.listen(8000, () => { console.log('server started'); });
