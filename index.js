31d0303bac7b54d8b22c5a2afbc732e0a57c2f3a
Patternlol <104591560+PaternalOil@users.noreply.github.com>
2022-12-26 17:52:45 +0000

    Added multiple ejs pages

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
+const ejs = require('ejs');     
+var fs = require('fs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+ const myCss = {
+  style : fs.readFileSync('./style.css','utf8')
+};
 
-app.get('/', (req, res) => { res.sendFile(path.join(__dirname + '/index.html')); });
+ const myCss2 = {
+  style : fs.readFileSync('./style2.css','utf8')
+};
 
-app.get('/', function(req, res) {
-  res.sendFile(path.join(__dirname + '/index.html'));
+ const myCss3 = {
+  style : fs.readFileSync('./videostyle.css','utf8')
+};
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set("view engine", "ejs");
+
+app.get('/', function (req, res) {
+  res.render('index.ejs', {
+  title: 'My Site',
+  myCss: myCss
+  });
 });
 
-app.listen(8000, () => { console.log('server started'); });
+app.get('/login', function (req, res) {
+  res.render('login.ejs', {
+  title: 'My Site',
+  myCss: myCss2
+  });
+});
+
+app.get('/videos', (req, res) => {
+  res.render('videos.ejs', {
+  title: 'My Site',
+  myCss: myCss3
+  });
+});
+
+app.post('/', (req, res) => {
+  console.log(req.body);
+});
+
+app.listen(8000, () => { console.log('// Server Start //'); });
+// sigill!!!
